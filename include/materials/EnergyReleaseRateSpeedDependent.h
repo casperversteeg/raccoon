5ed24da9da07f0859e8ea5005c7b1edced977ab2
Casper Versteeg <casper.versteeg@outlook.com>
2020-10-21 20:13:37 -0400

    Gonna have to fix all of this... :(

--- include/materials/EnergyReleaseRateSpeedDependent.h
+++ include/materials/EnergyReleaseRateSpeedDependent.h
@@ -5,9 +5,11 @@
 #pragma once
 
 #include "Material.h"
+#include "DerivativeMaterialPropertyNameInterface.h"
 
 template <bool is_ad>
-class EnergyReleaseRateSpeedDependentTempl : public Material
+class EnergyReleaseRateSpeedDependentTempl : public Material,
+                                             public DerivativeMaterialPropertyNameInterface
 {
 public:
   static InputParameters validParams();
@@ -15,14 +17,22 @@ public:
   EnergyReleaseRateSpeedDependentTempl(const InputParameters & parameters);
 
 protected:
+  virtual void initQpStatefulProperties() override;
   virtual void computeQpProperties() override;
 
   const GenericVariableValue<is_ad> & _v;
+  // const ADVariableValue & _d_dot;
+  // const ADVariableGradient & _grad_d;
   const Real & _Gc0;
   const Real & _v_lim;
 
   /// computed fracture energy release rate
   GenericMaterialProperty<Real, is_ad> & _Gc;
+  GenericMaterialProperty<Real, is_ad> & _dGc_dv;
+  GenericMaterialProperty<Real, is_ad> & _d2Gc_dv2;
+
+  /// Old Gc
+  const MaterialProperty<Real> & _Gc_old;
 };
 
 typedef EnergyReleaseRateSpeedDependentTempl<false> EnergyReleaseRateSpeedDependent;
